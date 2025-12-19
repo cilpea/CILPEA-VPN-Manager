1e4d59b1f4e4182d452ae5c0b6d9c0ad6e2e7690
CatRob satoshi Git <137897299+Catro-b@users.noreply.github.com>
2025-12-20 02:49:06 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import "@google/genai" from "/vercel/path0/App.tsx"
+import "@google/genai" 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
